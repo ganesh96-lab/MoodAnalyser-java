86c85387bcb84b92d0cfcaad2566f303c6b2c05c
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-03 13:11:04 -0400

    REFACTORED: Code to throw MoodAnalysisException given improper class name and reduce repeated code

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.moodanalyser;
 import com.bridgelabz.moodanalyser.exceptions.MoodAnalysisException;
+import factory.com.bridgelabz.moodanalyser.MoodAnalyserFactory;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -45,48 +46,20 @@ public class MoodAnalyserTest
 
     @Test
     public void givenMoodAnalyser_whenProper_shouldReturnObject() {
-        try {
-            Constructor constructor = Class.forName("com.bridgelabz.moodanalyser.MoodAnalyser").getConstructor();
-            Object reflectionObject = constructor.newInstance();
-            MoodAnalyser moodAnalyser = (MoodAnalyser) reflectionObject;
-            MoodAnalyser realMoodObject = new MoodAnalyser();
-            Assert.assertTrue(realMoodObject.equals(moodAnalyser));
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
+        
+            MoodAnalyser moodAnalyserObject = MoodAnalyserFactory.getMoodAnalyserObject();
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Assert.assertTrue(moodAnalyser.equals(moodAnalyserObject));
     }
 
     @Test
     public void givenMoodAnalyser_whenImProper_shouldThrowMoodAnalysisException() {
-        try {
-            Constructor constructor = Class.forName("com.bridgelabz.MoodAnalyser").getConstructor();
-            Object reflectionObject = constructor.newInstance();
-            MoodAnalyser moodAnalyser = (MoodAnalyser) reflectionObject;
-            MoodAnalyser realMoodObject = new MoodAnalyser();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            try {
-                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.CLASS_NOT_FOUND,
-                        "Invalid class name");
-            }catch (MoodAnalysisException moodAnalysisException){
-                Assert.assertEquals(MoodAnalysisException.ExceptionType.CLASS_NOT_FOUND,
-                        moodAnalysisException.type);
-            }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+        MoodAnalyser moodAnalyserObject = null;
+        try{
+            moodAnalyserObject = MoodAnalyserFactory.getMoodAnalyserObject("com.bridgelabz.MoodAnalyser");
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+        } catch (MoodAnalysisException moodAnalysisException){
+            Assert.assertEquals("Invalid class name", moodAnalysisException.getMessage());
         }
     }
 
