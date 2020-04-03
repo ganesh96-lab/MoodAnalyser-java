291b63989dcea1aac5be4242b68afa7c5d7d4b35
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-04-03 12:24:23 -0400

    REFACTORED: Code to throw MoodAnalysisException given improper class …

--- src/main/java/factory/com/bridgelabz/moodanalyser/MoodAnalyserFactory.java
+++ src/main/java/factory/com/bridgelabz/moodanalyser/MoodAnalyserFactory.java
@@ -1,6 +1,7 @@
 package factory.com.bridgelabz.moodanalyser;
 
 import com.bridgelabz.moodanalyser.MoodAnalyser;
+import com.bridgelabz.moodanalyser.exceptions.MoodAnalysisException;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -16,6 +17,27 @@ public class MoodAnalyserFactory
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static MoodAnalyser getMoodAnalyserObject(String className, String... constParam)
+            throws MoodAnalysisException {
+        try {
+            Constructor constructor = Class.forName(className).getConstructor(String.class);
+            Object reflectionObject = constructor.newInstance(constParam);
+            return  (MoodAnalyser) reflectionObject;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.CLASS_NOT_FOUND,
+                    "Invalid class name");
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
